bd854fd31aca0557a5ad710ff138b9277e7d0fb4
Balogunolalere <Lordareello@gmail.com>
2020-08-23 01:56:29 +0100

    New update for Heroku deployment

--- shop/views.py
+++ shop/views.py
@@ -1,9 +1,12 @@
-from django.views.generic import TemplateView,ListView
+from django.views.generic import TemplateView,ListView,FormView
 from .models import List
-
+from .forms import ContactForm
+from django.urls import reverse_lazy
 # Create your views here.
-class HomePageView(TemplateView):
+class HomePageView(FormView):
+	form_class = ContactForm
 	template_name = 'index.html'
+	success_url = '/'
 
 class ListPageView(ListView):
 	model = List
--- sit/settings.py
+++ sit/settings.py
@@ -124,5 +124,16 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 
 django_heroku.settings(locals())
+
+
+#gmail_send/settings.py
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'lordareello@gmail.com'
+EMAIL_HOST_PASSWORD = 'endowed000000007' #past the key or password app here
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'default from email'
+
     
 
